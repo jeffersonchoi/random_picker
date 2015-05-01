87f1b44a44469568a1db2ddc5697dd1ed9f0e268
Jefferson Choi <jeffersonchoi@gmail.com>
2015-04-30 17:07:33 -0700

    button prnts string to console

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 wdi_16_names = ["Alexander Mireles", "Andre Trelles", "Andrew Ham", "Ari Kanamori", "Busyra Anwar", "Carlos Meza", "Chris Garvin", "Cristal Chavez", "Daley Meistrell", "Daniel Manuel", "Jefferson Choi", "Jessica Grinberg", "John Ward", "Kate Wood", "Kevin Mehrabi", "Max Tran", "Max Plisskin", "Megan Kakitsubo", "Meryl Harden", "Mike McCready", "Nick Dalke","Niels Bischoff", "Paul Aglione", "Rahul Sidhu", "Samuel Chun", "Samuel Amiri", "Sophie Chor","Steve Soter", "Taylor Adams", "Yo Wakita"]
 
+function namePicker() {
 var namePicker = wdi_16_names[Math.floor(Math.random() * wdi_16_names.length)];
 console.log(namePicker);
+}
